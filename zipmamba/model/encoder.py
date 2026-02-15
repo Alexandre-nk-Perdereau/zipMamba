3eba15d8ff974e67261cf014fb362002805683b3
Alexandre Perdereau <alexandre.perdereau@pm.me>
2026-02-15 13:07:08 +0100

    reverse skip change

--- zipmamba/model/encoder.py
+++ zipmamba/model/encoder.py
@@ -251,22 +251,19 @@ class EfficientASREncoder(nn.Module):
                 if x_lengths is not None:
                     x_lengths = self.upsamples[i].get_output_length(x_lengths)
 
+                if skip_connections:
+                    skip = skip_connections.pop()
+                    if skip.shape[-1] != x.shape[-1]:
+                        key = f"skip_{skip.shape[-1]}_to_{x.shape[-1]}"
+                        skip = self.skip_projs[key](skip)
+                    min_len = min(x.shape[1], skip.shape[1])
+                    x = x[:, :min_len, :] + skip[:, :min_len, :]
+                    if x_lengths is not None:
+                        x_lengths = x_lengths.clamp(max=min_len)
+
             stack_input = x
             for block in self.stacks[i]:
                 x = block(x)
-
-            if self.upsamples[i] is not None and skip_connections:
-                skip = skip_connections.pop()
-                if skip.shape[-1] != x.shape[-1]:
-                    key = f"skip_{skip.shape[-1]}_to_{x.shape[-1]}"
-                    skip = self.skip_projs[key](skip)
-                min_len = min(x.shape[1], skip.shape[1])
-                skip = skip[:, :min_len, :]
-                x = x[:, :min_len, :] + skip
-                stack_input = stack_input[:, :min_len, :] + skip
-                if x_lengths is not None:
-                    x_lengths = x_lengths.clamp(max=min_len)
-
             x = self.bypasses[i](stack_input, x)
 
             if return_intermediates and i == mid_point:
